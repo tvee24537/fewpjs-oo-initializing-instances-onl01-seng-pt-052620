05d5a361b2842da0c36dbcdfef1594517cee7729
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-26 20:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+    constructor (food, drink){
+        this.drink = drink;
+        this.food = food;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this._dessert = dessert
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+    }
+} 
